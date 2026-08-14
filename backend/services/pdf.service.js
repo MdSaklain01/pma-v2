cd5774c77aa97e2d606b38d57bfcb079a03b208b
mnkhan78 <naseem.khan9500078@gmail.com>
2026-08-14 18:38:18 +0530

    puppeteer fix 2

--- backend/services/pdf.service.js
+++ backend/services/pdf.service.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
 
 const generatePDF = async (html) => {
     const { default : puppeteer } = await import("puppeteer");
