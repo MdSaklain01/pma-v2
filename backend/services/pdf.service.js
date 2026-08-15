f814dcbe55edfdc080b0b87cae02a29370267298
mnkhan78 <naseem.khan9500078@gmail.com>
2026-08-15 12:10:36 +0530

    change in the docker file for puppeteer to work in docker container

--- backend/services/pdf.service.js
+++ backend/services/pdf.service.js
@@ -4,13 +4,14 @@ const generatePDF = async (html) => {
     const { default : puppeteer } = await import("puppeteer");
     const browser = await puppeteer.launch({
         headless: true,
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            // "--disable-accelerated-2d-canvas",
-            "--disable-gpu",
-        ],
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        // args: [
+        //     "--no-sandbox",
+        //     "--disable-setuid-sandbox",
+        //     "--disable-dev-shm-usage",
+        //     // "--disable-accelerated-2d-canvas",
+        //     "--disable-gpu",
+        // ],
     });
 
     try {
